5df88329147a72024939c61165a68b9a9b2a5734
Mav-isolivr <olivr.mavis@gmail.com>
2025-05-26 22:20:07 -0300

    aulas de React, JSX e CSS

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>NyanCats são fofos e legais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, necessitatibus facere voluptatibus consequuntur amet quisquam veniam tempore. Nostrum eligendi ducimus quisquam, illo, ullam quas praesentium, sapiente aperiam commodi odit consequuntur!</p>
+    </div>
+  )
+}
+
+export default Body
---
+++ ola/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='Header'>
+        <h1>🦄 Unicórnios 🦄</h1>
+    </div>
+  )
+}
+
+export default Header
