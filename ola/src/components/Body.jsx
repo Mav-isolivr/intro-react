9b05d1d31f0bdfc96a00d701b39de0ef8efa3ffe
MARIA VITHÓRIA DE OLIVEIRA <maria_oliveira159@estudante.sesisenai.org.br>
2025-05-27 21:50:16 -0300

    React e UsesStates

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { useState } from 'react'
 import './Body.css'
 
 function Body() {
+  
+  const[cont, setCont] = useState(0)
+  const[inputValor, setInputValor] = useState('')
+  const[inputN1, setInputN1] = useState('')
+  const[inputN2, setInputN2] = useState('')
+
+  function more(){
+    setCont(cont + 1)
+  }
+
+  function less(){
+    if(cont > 0){
+    setCont(cont - 1)
+  }
+  }
+
+  function alterar(){
+    setCont(Number(inputValor))
+  }
+
   return (
     <div className='Body'>
         <h2 className='titulo2'>NyanCats são fofos e legais</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, necessitatibus facere voluptatibus consequuntur amet quisquam veniam tempore. Nostrum eligendi ducimus quisquam, illo, ullam quas praesentium, sapiente aperiam commodi odit consequuntur!</p>
+        
+        <div className='cont'>
+         <button onClick={more}>+</button>
+         <p>Quantos gatinhos fofos e estupidamente coloridos irá alegrar seu dia de hoje: ^{cont}^</p>
+         <button onClick={less}>-</button>
+         </div>
+
+         <div className='form'>
+          <input type="number" 
+          value={inputValor}
+          onChange={(e) => setInputValor(e.target.value)}
+          />
+          <button onClick={alterar}>Alterar</button>
+         </div>
+
+         <div className='calculadora'>
+          <input type="text" 
+          value={inputN1}
+          onChange={(e) => setInputN1(e.target.value)}
+          />
+            
+          <button>+</button>
+          <input type="text"
+          value={inputN2}
+          onChange={(e) => setInputN2(e.target)}
+          /> {inputN2}
+         </div>
     </div>
   )
 }
