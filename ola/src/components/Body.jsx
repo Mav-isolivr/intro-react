821e55c2fe88cbdb7365bafdff17e192aacecde9
MARIA VITHÓRIA DE OLIVEIRA <maria_oliveira159@estudante.sesisenai.org.br>
2025-05-28 21:54:56 -0300

    feat: estilização css e interação de apiHP

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,59 +1,28 @@
-import { useState } from 'react'
-import './Body.css'
+import "./Body.css";
 
-function Body() {
-  
-  const[cont, setCont] = useState(0)
-  const[inputValor, setInputValor] = useState('')
-  const[inputN1, setInputN1] = useState('')
-  const[inputN2, setInputN2] = useState('')
-
-  function more(){
-    setCont(cont + 1)
-  }
-
-  function less(){
-    if(cont > 0){
-    setCont(cont - 1)
-  }
-  }
-
-  function alterar(){
-    setCont(Number(inputValor))
-  }
+function Body(props) {
+  console.log(props.characters);
 
   return (
-    <div className='Body'>
-        <h2 className='titulo2'>NyanCats são fofos e legais</h2>
-        
-        <div className='cont'>
-         <button onClick={more}>+</button>
-         <p>Quantos gatinhos fofos e estupidamente coloridos irá alegrar seu dia de hoje: ^{cont}^</p>
-         <button onClick={less}>-</button>
-         </div>
-
-         <div className='form'>
-          <input type="number" 
-          value={inputValor}
-          onChange={(e) => setInputValor(e.target.value)}
-          />
-          <button onClick={alterar}>Alterar</button>
-         </div>
-
-         <div className='calculadora'>
-          <input type="text" 
-          value={inputN1}
-          onChange={(e) => setInputN1(e.target.value)}
-          />
-            
-          <button>+</button>
-          <input type="text"
-          value={inputN2}
-          onChange={(e) => setInputN2(e.target)}
-          /> {inputN2}
-         </div>
+    <div className="Body">
+      
+      <div className="grid">
+      {props.characters.map((character) => (
+        <div className="card">
+          {character.image ?
+           (<img src={character.image} alt={character.name}/>) : (
+            <div className="noImage">Imagem Indisponível</div>
+           )}
+    
+          <h2>{character.name}</h2>
+          <p><strong>Casa:</strong> {character.house || "Desconhecida"}</p>
+        </div>
+      ))}
+      </div>
     </div>
   )
 }
 
 export default Body
+
+
--- ola/src/components/Header.jsx
+++ ola/src/components/Header.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
+  const [searchTerm, setSearchTerm] = React.useState('')
   return (
     <div className='Header'>
-        <h1>🦄 Unicórnios 🦄</h1>
+        <h1>Buscador de Personagens - Harry Potter</h1>
+        
+        <input
+         type="text" 
+         placeholder='Digite o nome...'
+         value={searchTerm}
+         onChange={(e) => setSearchTerm(e.target.value)}
+         className='search'/>
     </div>
   )
 }
 
 export default Header
+
